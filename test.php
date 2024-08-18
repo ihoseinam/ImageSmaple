d56b748183a6b23a80d5ef34624da8b36740ed82
Hosein <124840513+ihoseinam@users.noreply.github.com>
2024-08-18 23:59:04 +0330

    Update test.php

--- test.php
+++ test.php
@@ -1,9 +1,5 @@
-<?php
-header('Content-Type: application/json');
-
-$response = [
-    'status' => 'success',
-    'message' => 'This is a simple JSON response.',
-];
-
-echo json_encode($response);
+{
+data=[],
+message="success",
+status="ok"
+}
